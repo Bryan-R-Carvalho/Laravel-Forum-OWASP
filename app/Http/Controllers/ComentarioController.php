84e6b1725797968bc672f79d143865eec73bd168
Bryan <reisdecarvalhobryan@gmail.br>
2024-02-14 01:35:38 -0300

    mudanca de pradronizaçao

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -19,7 +19,7 @@ class ComentarioController extends Controller
 
     public function index(){
         $comentarios = DB::table('comentarios')
-            ->join('users', 'comentarios.id_user', '=', 'users.id')
+            ->join('users', 'comentarios.user_id', '=', 'users.id')
             ->select('comentarios.*', 'users.name')
             ->orderBy('created_at', 'desc')
             ->get();
@@ -43,7 +43,7 @@ class ComentarioController extends Controller
         $request->validate([
             'conteudo' => 'required|max:120'
         ]);
-        $request->merge(['id_user' => $user,'likes' => 0, 'id_comentario' => null, 'users_like' => []]);
+        $request->merge(['user_id' => $user, 'ativo' => true, 'comentario_id' => null]);
        
         $this->comentario->create($request->all());
 
@@ -68,56 +68,5 @@ class ComentarioController extends Controller
         return redirect()->route('comentarios.index');
     }
 
-    public function like($id){
-        $comentario = $this->comentario->find($id);
-        $users_like = $comentario->users_like;
 
-        $index = $this->binarySearch($users_like, auth()->user()->id);
-        
-        if($index === -1){
-            $comentario->update(['likes' => $comentario->likes + 1]);
-            $users_like[] = auth()->user()->id;
-
-        }else{
-            unset($users_like[$index]);
-            $comentario->update(['likes' => $comentario->likes - 1]);
-            $users_like = array_values($users_like);
-        }
-
-        $comentario->update(['users_like' => $users_like]);
-        return back();
-    }
-
-    public function binarySearch($array, $target) {
-        $left = 0;
-        $right = count($array) - 1;
-        while ($left <= $right) {
-            $mid = floor(($left + $right) / 2);
-
-            if ($array[$mid] == $target) {
-                return $mid;
-            }
-            if ($array[$mid] < $target) {
-                $left = $mid + 1;
-            } else {
-                $right = $mid - 1;
-            }
-        }
-        return -1;
-    }
-       
-    // public function resposta(Request $request, $id){
-    //     //dd($request->all());
-    //     $user = auth()->user()->id;
-    //     $comentario = $this->comentario->find($id);
-    //     //dd($comentario->id);
-    //     $request->validate([
-    //         'conteudo' => 'required|max:120'
-    //     ]);
-    //     $request->merge(['id_user' => $user,'likes' => 0, 'id_comentario' => $comentario->id]);
-       
-    //     $this->comentario->create($request->all());
-
-    //     return back();
-    // }
 }
