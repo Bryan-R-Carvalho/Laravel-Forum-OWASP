c25e84bdaa6747cfd122294165d31de738da1f03
Bryan-R-Carvalho <reisdecarvalhobryan@gmail.com>
2024-03-31 20:01:52 -0300

    add o plugin intersect alpine

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
 
-// import Alpine from 'alpinejs';
+import Alpine from 'alpinejs';
+import intersect from '@alpinejs/intersect'
+ 
+Alpine.plugin(intersect)
 
-// window.Alpine = Alpine;
+window.Alpine = Alpine;
 
-// Alpine.start();
+Alpine.start();
